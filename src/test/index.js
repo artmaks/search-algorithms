3923353ac033dbd8787f5c737907f214212ac919
Artem Maksimov <temamaksimov@gmail.com>
2017-09-01 01:18:40 +0300

    Add test case for large array with the element at the end (exception for recursive)

--- src/test/index.js
+++ src/test/index.js
@@ -7,7 +7,8 @@ const listOfSortFunctions = [linearSearchRecursive, linearSearch]
 
 describe('Testing of all search algorithms', function () {
   listOfSortFunctions.forEach((sortFunction) => {
-    describe(`testing ${sortFunction.name}` , function () {
+    const functionName = sortFunction.name; 
+    describe(`testing ${functionName}` , function () {
 
       describe('simple static testing', function () {
         it('should find value 3 at index 2', function () {
@@ -19,11 +20,32 @@ describe('Testing of all search algorithms', function () {
         arrays.forEach((arr) => {
           arr.answers.forEach((answer) => {
             it(`should find value ${answer.item} at index ${answer.index}`, function () {
+              console.time('timer');
               assert.equal(answer.index, sortFunction(arr.array, answer.item))
+              console.timeEnd('timer');
             });
           })
         })
-      })
+      });
+
+      describe('large array (10000 items) with element at the end', function () {
+        const largeArray = new Array(999999).fill(999);
+        const item = 1000;
+        largeArray[largeArray.length - 1] = item;
+        const answer = largeArray.indexOf(item);
+
+        it(`should find value ${item} at index ${answer}`, function () {
+          console.time('timer');
+          if(functionName === 'linearSearchRecursive') {
+            assert.equal('RangeError', sortFunction(largeArray, item).name)
+            console.error('RangeError')
+          } else {
+            assert.equal(answer, sortFunction(largeArray, item))
+          }
+          console.timeEnd('timer');
+        });
+
+      });
 
     });
   })
