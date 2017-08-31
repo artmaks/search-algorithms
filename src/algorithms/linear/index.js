d5e802cbc689be21b34e70f5002461c1c4d58aef
Artem Maksimov <temamaksimov@gmail.com>
2017-08-31 23:54:26 +0300

    add simple linear search algorithm

--- src/algorithms/linear/index.js
+++ src/algorithms/linear/index.js
@@ -12,6 +12,22 @@ function linearSearchRecursiveWithRange(array, low, high, item) {
   return linearSearchRecursiveWithRange(array, low + 1, high, item);
 }
 
+function linearSearch(array, item) {
+  const min = 0;
+  const max = array.length;
+  return linearSearchWithRange(array, min, max, item);
+}
+
+function linearSearchWithRange(array, min, max, item) {
+  while(max > min) {
+    if(array[min] === item)
+      return min;
+    min++;
+  }
+  return -1;
+}
+
 export {
-  linearSearchRecursive
+  linearSearchRecursive,
+  linearSearch
 }
