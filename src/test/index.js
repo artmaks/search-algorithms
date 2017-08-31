e9f8a5ae6680fbf495bb643065412be07e339353
Artem Maksimov <temamaksimov@gmail.com>
2017-08-31 23:55:07 +0300

    run the same test for all search algorithms

--- src/test/index.js
+++ src/test/index.js
@@ -1,26 +1,30 @@
 import assert from 'assert'
 
-import { linearSearchRecursive } from '../algorithms/linear'
+import { linearSearchRecursive, linearSearch } from '../algorithms/linear'
 import { arrays } from './data'
 
-describe('Testing of search algorithms ', function () {
-  describe('simple static testing', function () {
+const listOfSortFunctions = [linearSearchRecursive, linearSearch]
 
-    it('should find value 3 at index 2', function () {
-      assert.equal(2, linearSearchRecursive([1, 2, 3], 3))
-    })
+describe('Testing of all search algorithms', function () {
+  listOfSortFunctions.forEach((sortFunction) => {
+    describe(`testing ${sortFunction.name}` , function () {
 
-  });
-
-  describe('static testing from prepared data file', function () {
-
-    arrays.forEach((arr) => {
-      arr.answers.forEach((answer) => {
-        it(`should find value ${answer.item} at index ${answer.index}`, function () {
-          assert.equal(answer.index, linearSearchRecursive(arr.array, answer.item))
-        });
+      describe('simple static testing', function () {
+        it('should find value 3 at index 2', function () {
+          assert.equal(2, sortFunction([1, 2, 3], 3))
+        })
+      });
+    
+      describe('static testing from prepared data file', function () {
+        arrays.forEach((arr) => {
+          arr.answers.forEach((answer) => {
+            it(`should find value ${answer.item} at index ${answer.index}`, function () {
+              assert.equal(answer.index, sortFunction(arr.array, answer.item))
+            });
+          })
+        })
       })
-    })
 
+    });
   })
 });
